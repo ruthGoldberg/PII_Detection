2266a9bd7f14ede655971f4bfa889407912db96e
noyrosenbaum <noyrosen123@gmail.com>
2024-10-10 17:15:04 +0300

    Adding OCR functionality

--- YOLO-NAS/8_OCR_inference.py
+++ YOLO-NAS/8_OCR_inference.py
@@ -2,6 +2,9 @@ import os
 import cv2
 from ultralytics import YOLO
 import datetime
+import easyocr
+import re
+import matplotlib.pyplot as plt
 
 
 start = datetime.datetime.now()
@@ -23,7 +26,63 @@ model_path = os.path.join('.', 'runs', 'detect', 'train', 'weights', 'last.pt')
 model = YOLO(model_path)
 
 # Threshold for object detection
-threshold = 0.3
+threshold_YOLO = 0.3
+
+# Function to perform object detection using YOLO
+def detect_objects(image):
+    return model(image)[0]
+
+# Threshold for OCR
+threshold_OCR = 0.12
+
+# Initialize OCR reader
+reader = easyocr.Reader(['en'])
+
+# Function to recognize text in an image using OCR
+def recognize_text(image):
+    return reader.readtext(image)
+
+def WriteLabels(labels, labels_filepath):        
+    with open(labels_filepath, 'w') as txt_file:
+        for label in labels:
+            txt_file.write(label + '\n')
+
+# Function to perform OCR on an image and save the processed image - DEVIDE TO OCR PER CLASS
+def perform_ocr_on_image(image, labels_filepath):
+    labels = []
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = recognize_text(image_rgb)
+
+    
+    for (bbox, text, prob) in results:
+        if prob >= threshold_OCR:
+            (top_left, _, bottom_right, _) = bbox
+            top_left = (int(top_left[0]), int(top_left[1]))
+            bottom_right = (int(bottom_right[0]), int(bottom_right[1]))
+
+            num = text.strip()
+            nuk = len(num)
+
+            credit_card_pattern = r'^\d{16}$'
+            card_expiry = '\d{2}/\d{2}'
+            card_name = '^[a-zA-Z]+(?: [a-zA-Z]+)?$'
+
+            if nuk >= 4 and sum(c.isdigit() for c in num) >= 2:
+                cv2.rectangle(image, pt1=top_left, pt2=bottom_right, color=(0, 255, 0), thickness=-1)
+                cv2.putText(image, 'card_number', (top_left[0], top_left[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+                labels.append(f"1 {top_left[0] / 640} {top_left[1] / 640} {bottom_right[0] / 640} {bottom_right[1] / 640}")
+            elif re.findall(card_expiry, num):
+                cv2.rectangle(image, pt1=top_left, pt2=bottom_right, color=(0, 255, 0), thickness=-1)
+                cv2.putText(image, 'exp_date', (top_left[0], top_left[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+                labels.append(f"2 {top_left[0] / 640} {top_left[1] / 640} {bottom_right[0] / 640} {bottom_right[1] / 640}")
+
+            # elif re.findall(card_name, num):
+            #     cv2.rectangle(image, pt1=top_left, pt2=bottom_right, color=(0, 255, 0), thickness=-1)
+            #     cv2.putText(image, 'holder_name', (top_left[0], top_left[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+            #     labels.append(f"4 {top_left[0]} {top_left[1]} {bottom_right[0]} {bottom_right[1]}")
+
+    #WriteLabels(labels, labels_filepath)
+    return (image, labels)
 
 # Text color dictionary for different labels
 text_colors = {
@@ -52,10 +111,12 @@ for image_name in os.listdir(images_dir):
             continue
 
         # Perform object detection
-        results = model(image)[0]
+        results = detect_objects(image)
 
         # Initialize labels for the current image
         labels = []
+        # Initialize a flag to keep track of the detected classes other than front_side and back_side
+        other_classes_detected = False
 
         image_height, image_width, _ = image.shape
 
@@ -63,7 +124,7 @@ for image_name in os.listdir(images_dir):
         for result in results.boxes.data.tolist():
             x1, y1, x2, y2, score, class_id = result
 
-            if score > threshold:
+            if score > threshold_YOLO:
                 x_center = (x1 + x2) / (2 * image_width)
                 y_center = (y1 + y2) / (2 * image_height)
                 box_width = (x2 - x1) / image_width
@@ -74,7 +135,7 @@ for image_name in os.listdir(images_dir):
 
                 # Get the class name
                 class_name = results.names[int(class_id)].lower()
-
+                other_classes_detected = True
                 #Check if the class is "card_number" or "exp_date"
                 if class_name in class_names:
                     # Draw rectangle around the detected object
@@ -89,6 +150,11 @@ for image_name in os.listdir(images_dir):
                     #             cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
                     cv2.rectangle(image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 0), -1)
                     
+        if not other_classes_detected:
+            # Perform OCR on the image and save the processed image
+            ocr_image, ocr_labels = perform_ocr_on_image(image, labels_filepath)
+            labels.extend(ocr_labels)
+
         # Save the processed image to the output directory
         #output_image_path = os.path.join(output_dir, image_name)
         cv2.imwrite(output_filepath, image)
